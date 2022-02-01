66762b16ebc4a0aa01ccb334692ee39a309764ee
medansari <m3dansari@hotmail.com>
2022-02-01 21:13:07 +0000

    Adding definitions and setup

--- Assignment_1.ino
+++ Assignment_1.ino
@@ -1,7 +1,17 @@
-void setup() {
- // initialize digital pin LED_BUILTIN as an output.
-  pinMode(21, OUTPUT);
+#define button_1 22
+#define button_2 23
+#define signal_A 15
+#define signal_B 21
+#define a 100
+#define b 1300
+#define c 12
+#define d 500
 
+void setup() {
+  pinMode(signal_A, OUTPUT);
+  pinMode(signal_B, OUTPUT);
+  pinMode(button_1, INPUT);
+  pinMode(button_2, INPUT);
 }
 
 void loop() {
