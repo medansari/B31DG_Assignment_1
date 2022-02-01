3ec8f98193f4aa4c680c3bf2de564034b07f5d91
medansari <m3dansari@hotmail.com>
2022-02-01 21:08:02 +0000

    Testing LED

--- Assignment_1.ino
+++ Assignment_1.ino
@@ -1,9 +1,13 @@
 void setup() {
-  // put your setup code here, to run once:
+ // initialize digital pin LED_BUILTIN as an output.
+  pinMode(21, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(21, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(21, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);   
 
 }
